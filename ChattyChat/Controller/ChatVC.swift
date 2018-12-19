0a66f4cb9c761416a8ff197e637fc0c14d2ab6e8
Roya Rastgar <royarastgar@Royas-Mac.local>
2018-12-19 02:07:27 -0800

    SWRevealViewController is successfully done

---
+++ ChattyChat/Controller/ChatVC.swift
@@ -0,0 +1,38 @@
+//
+//  ChatVC.swift
+//  ChattyChat
+//
+//  Created by Roya Rastgar on 2018-12-19.
+//  Copyright © 2018 Roya Rastgar. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
